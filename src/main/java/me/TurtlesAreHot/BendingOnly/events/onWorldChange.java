3b882ad49c94471ce163ddf0b556b0b9a4555c07
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-12-07 17:41:51 -0500

    Add files via upload

--- src/main/java/me/TurtlesAreHot/BendingOnly/events/onWorldChange.java
+++ src/main/java/me/TurtlesAreHot/BendingOnly/events/onWorldChange.java
@@ -3,6 +3,7 @@ package me.TurtlesAreHot.BendingOnly.events;
 import me.TurtlesAreHot.BendingOnly.Config;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
@@ -23,10 +24,7 @@ public class onWorldChange implements Listener {
             // Slot 40 is offhand
             if(!(Config.isArmorAllowed())) {
 
-                if(contents[39] != null ||
-                contents[38] != null ||
-                contents[37] != null ||
-                contents[36] != null) {
+                if(hasArmorOn(e.getPlayer())) {
                     // Basically checks if any of the armor pieces are filled with something
                     e.getPlayer().setHealth(0.0);
                     e.getPlayer().sendMessage(ChatColor.RED + "You cannot enter the arena while wearing armor. Please remove it.");
@@ -43,6 +41,92 @@ public class onWorldChange implements Listener {
                 }
             }
 
+        }
+    }
+
+    public boolean hasArmorOn(Player p) {
+        ItemStack[] contents = p.getInventory().getContents();
+        ItemStack helm = contents[39];
+        ItemStack chest = contents[38];
+        ItemStack leg = contents[37];
+        ItemStack boot = contents[36];
+        if(helm != null) {
+            if(isArmor(helm.getType())) {
+                return true;
+            }
+        }
+        if(chest != null) {
+            if(isArmor(chest.getType())) {
+                return true;
+            }
+        }
+        if(leg != null) {
+            if (isArmor(leg.getType())) {
+                return true;
+            }
+        }
+        if(boot != null) {
+            if(isArmor(boot.getType())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isArmor(Material m) {
+        switch(m) {
+            case NETHERITE_HELMET:
+                return true;
+            case NETHERITE_CHESTPLATE:
+                return true;
+            case NETHERITE_LEGGINGS:
+                return true;
+            case NETHERITE_BOOTS:
+                return true;
+            case DIAMOND_HELMET:
+                return true;
+            case DIAMOND_CHESTPLATE:
+                return true;
+            case DIAMOND_LEGGINGS:
+                return true;
+            case DIAMOND_BOOTS:
+                return true;
+            case GOLDEN_HELMET:
+                return true;
+            case GOLDEN_CHESTPLATE:
+                return true;
+            case GOLDEN_LEGGINGS:
+                return true;
+            case GOLDEN_BOOTS:
+                return true;
+            case CHAINMAIL_HELMET:
+                return true;
+            case CHAINMAIL_CHESTPLATE:
+                return true;
+            case CHAINMAIL_LEGGINGS:
+                return true;
+            case CHAINMAIL_BOOTS:
+                return true;
+            case IRON_HELMET:
+                return true;
+            case IRON_CHESTPLATE:
+                return true;
+            case IRON_LEGGINGS:
+                return true;
+            case IRON_BOOTS:
+                return true;
+            case LEATHER_HELMET:
+                return true;
+            case LEATHER_CHESTPLATE:
+                return true;
+            case LEATHER_LEGGINGS:
+                return true;
+            case LEATHER_BOOTS:
+                return true;
+            case ELYTRA:
+                return true;
+            default:
+                return false;
         }
     }
 
